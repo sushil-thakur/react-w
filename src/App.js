597904566ae22383a7fbc8ebd8eb6717fa90bb33
sushil thakur <sushilt2059@gmail.com>
2024-12-11 18:31:41 +0545

    dark

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
+import React, { useState } from 'react';
 
 function App() {
-  const [darkmode, setdarkmode] = useState(false);//whether dark mode is wnable or not
+  const [mode, setdarkmode] = useState('dark');//whether dark mode is wnable or not
+   const toggleMode=()=>{
+    if(mode==='light'){
+      setdarkmode('dark');
+      document.body.style.backgroundColor='black';
+    }
+    else{
+      setdarkmode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
   return (
     <>
-<Navbar title="TextUtlis" about="About us" mode={darkmode}/>
+<Navbar title="TextUtlis" about="About us" mode={mode} toggleMode={toggleMode}/>
 {/* <Navbar/> */}
 <div className="container my-3">
-<TextForm heading="Enter the text to analyze"/>
+<TextForm heading="Enter the text to analyze" mode={mode}/>
 {/* <About/> */}
 </div>
     </>

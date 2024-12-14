e7b3a1cc54aed9bdcbc3c5f7c70a454d62d0a8cc
sushil thakur <sushilt2059@gmail.com>
2024-12-14 19:11:44 +0545

    push

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
-// import logo from './logo.svg';
 import './App.css';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-  
-  const [mode, setdarkmode] = useState('dark');//whether dark mode is wnable or not
+  const [mode, setdarkmode] = useState('dark'); // whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
-  const showAlert=(message,type)=>{
+
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
-    })
+      msg: message,
+      type: type
+    });
     setTimeout(() => {
       setAlert(null);
-    }, 1500);}
+    }, 1500);
+  };
 
-   const toggleMode=()=>{
-    if(mode==='light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setdarkmode('dark');
-      document.body.style.backgroundColor='black';
-      showAlert("Dark mode has been enabled","success");
-      document.title='TextUtlis-Dark';
-    }
-    else{
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark';
+    } else {
       setdarkmode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("Light mode has been enabled","success");
-      document.title='TextUtlis-light';
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.title = 'TextUtils - Light';
     }
-  }
+  };
+
   return (
     <>
-<Navbar title="TextUtlis" about="About us" mode={mode} toggleMode={toggleMode}/>
-{/* <Navbar/> */}
-<Alert alert={alert}/>
-<div className="container my-3">
-<TextForm  showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
-{/* <About/> */}
-<colorPalette/>
-</div>
+      <Router>
+        <Navbar title="TextUtils" about="About us" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
- 
-    
-        
-      
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
@@ -12,12 +13,13 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" href="/">Home</a>
+              <Link className="nav-link active" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">{props.about}</a>
+              <Link className="nav-link" to="/About">{props.about}</Link>
             </li>
           </ul>
+          
           <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
             <input
               className="form-check-input"
